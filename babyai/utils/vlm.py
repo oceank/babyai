d72bd78d735953cbcee378705e3ada30d5148731
Jianhai Su <suj@email.sc.edu>
2023-01-10 17:59:18 -0500

    utilify functions for chehcking the performance of a trained vlm

--- babyai/utils/vlm.py
+++ babyai/utils/vlm.py
@@ -1,3 +1,4 @@
+import os
 import time
 import datetime
 import gc
@@ -6,6 +7,7 @@ import torch
 import torch.nn as nn
 from torch.cuda import amp
 import numpy as np
+import matplotlib.pyplot as plt
 
 from ..model import ImageBOWEmbedding, initialize_parameters
 
@@ -502,3 +504,245 @@ def train_test_helper(
 
     loss_statistics = get_stat(losses, rd=4, return_type='np')
     return loss_statistics
+
+def load_losses(models_dir, exp_name):
+
+    train_loss_fn = "train_loss.npy"
+    test_loss_fn = "test_loss.npy"
+    model_dir=models_dir + "/" + exp_name
+
+    train_loss = np.load(os.path.join(model_dir, train_loss_fn))
+    test_loss = np.load(os.path.join(model_dir, test_loss_fn))
+
+    return train_loss, test_loss
+
+def plot_losses(title, train_loss, test_loss, num_epochs, fig_fp=None):
+
+    epochs = range(1, num_epochs+1)
+
+    plt.plot(epochs,train_loss, label="train loss")
+    plt.plot(epochs,test_loss, label="test loss")
+
+    plt.title(title)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+
+    if fig_fp:
+        plt.savefig(fig_fp)
+
+def test_subgoal_generation(demo, vlm, img_encoder, device, abstract_history, lowlevel_instr_set, tokenizer):
+
+    vlm.to(device)
+    img_encoder.to(device)
+    vlm.eval()
+    img_encoder.eval()
+    seed = demo[-1]
+    losses = []
+
+    msg = "Proposed Subgoal VS Target Subgoal:\n"
+
+    obss, csg_texts, csg_time_steps, pre_csg_time_steps = prepare_input_seq(
+        demo, abstract_history, lowlevel_instr_set)
+
+    csg_texts_tokens = tokenizer(csg_texts, padding=True, return_tensors="pt")
+    for key in csg_texts_tokens:
+        csg_texts_tokens[key] = csg_texts_tokens[key].to(device)
+    csg_tokens_len = csg_texts_tokens['attention_mask'].sum(dim=-1)
+
+    with torch.no_grad():
+        img_embeds = img_encoder(obss)
+
+    num_csgs = len(csg_time_steps)
+
+    mission = demo[0][0]['mission']
+    vlm_input = tokenizer([f"Goal: {mission}"], max_length=1024, padding="max_length", return_tensors='pt')
+    for key in vlm_input:
+        vlm_input[key] = vlm_input[key].to(device)
+    pre_input_text_len = vlm_input['attention_mask'].sum(dim=-1)[0] # batch size is 1 here
+
+    vlm_input['media_locations'] = torch.zeros(vlm_input['input_ids'].shape, dtype=torch.bool, device=device)
+
+    pre_pre_csg_time_step = -1
+    # Calculate the loss of each predicted subgoal by accumulatly preparing the sample input and label
+    #   Sample Input:
+    #       text: '<image...image>" tokens that correspond to previous subgoal or the intial observation
+    #       media: image embedding of the corresponding visual observations
+    #   Sample label: tokens of target/completed subgoal
+    for idx, pre_csg_time_step in zip(range(num_csgs), pre_csg_time_steps):
+        # set up labels for the current subgoal
+        num_attended_vis_obss = pre_csg_time_step - pre_pre_csg_time_step
+        if abstract_history:
+            num_attended_vis_obss = 1
+        
+        # Setup input ids and attention mask for the sample case of the new subgoal
+        #input_text_len = pre_input_text_len + csg_tokens_len[idx]
+        media_tokens_len = 2 + num_attended_vis_obss
+        pre_subgoal_status_token_len = 0
+        pre_subgoal_status_token_len = 3 # subgoal status "[Success]" => "[", "Success", "]"
+        '''
+        if pre_csg_time_step != 0:
+            pre_subgoal_status_token_len = 3 # subgoal status "[Success]" => "[", "Success", "]"
+        '''
+        added_input_tokens_len = media_tokens_len + pre_subgoal_status_token_len
+        input_text_len = pre_input_text_len + added_input_tokens_len
+        vlm_input['input_ids'][0, pre_input_text_len:input_text_len] = csg_texts_tokens['input_ids'][idx, :added_input_tokens_len]
+        vlm_input['attention_mask'][0, pre_input_text_len:input_text_len] = 1
+
+        # set up media and its locations (that correspond to previous subgoal) that are used to predit the current subgoal
+        current_media_locations = [pre_input_text_len]
+        if abstract_history:
+            vlm_input['image_embeds'] = img_embeds[:, :(idx+1), :, :]
+        else:
+            # Instead of the 1st 'image', '<' in '<image...image>' corresonds to the 1st media location
+            current_media_locations.extend([pre_input_text_len+i for i in range(2, num_attended_vis_obss+1)])
+            vlm_input['image_embeds'] = img_embeds[:, :(pre_csg_time_step+1), :, :]
+        vlm_input['media_locations'][0, current_media_locations] = True
+
+        # Calculate the loss and update the model
+        max_len_generated_sent = 20
+        sentence_ending_token = 0 # token 0 is for comma, '!'
+        with torch.no_grad():
+            generated_tokens = vlm.generate_sentences(
+                max_len_generated_sent,
+                vlm_input['input_ids'],
+                vlm_input['attention_mask'],
+                image_embeds = vlm_input['image_embeds'],
+                media_locations = vlm_input['media_locations'],
+                sentence_ending_token = sentence_ending_token,
+                pad_token_id = tokenizer.pad_token_id
+                )
+        generated_sentence = tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
+        context_sentence = tokenizer.decode(vlm_input['input_ids'][0], skip_special_tokens=True)
+        completed_subgoal_text = tokenizer.decode(csg_texts_tokens['input_ids'][idx, added_input_tokens_len:csg_tokens_len[idx]], skip_special_tokens=True)
+        msg += f"Timestep {pre_csg_time_step}:\n"
+        msg += f"\tPrediction Context: {context_sentence}\n"
+        msg += f"\tProposed   Subgoal: {generated_sentence}\n"
+        msg += f"\tTarget     Subgoal: {completed_subgoal_text}\n"
+        
+        # Append the target subgoal tokens to input text tokens for assisting the generation of the next subgoal
+        input_text_len = pre_input_text_len + csg_tokens_len[idx]
+        vlm_input['input_ids'][0, pre_input_text_len:input_text_len] = csg_texts_tokens['input_ids'][idx, :csg_tokens_len[idx]]
+        vlm_input['attention_mask'][0, pre_input_text_len:input_text_len] = 1
+
+        # Update some auxilary variables
+        pre_input_text_len = input_text_len
+        pre_pre_csg_time_step = pre_csg_time_step
+
+    return msg
+
+def test_with_one_demo(demo, model, device, abstract_history, lowlevel_instr_set, tokenizer, skip_label):
+
+    vlm = model['vlm']
+    img_encoder = model['img_encoder']
+    vlm.to(device)
+    img_encoder.to(device)
+    vlm.eval()
+    img_encoder.eval()
+
+    msg = f"Demo Info:\n"
+    msg += f"\tGoal: {demo[0][0]['mission']}\n"
+    msg += f"\tCompleted Subgoals In Demo:\n"
+    obss, csg_texts, csg_time_steps, pre_csg_time_steps = prepare_input_seq(
+        demo, abstract_history, lowlevel_instr_set)
+    for count, csg_text in enumerate(csg_texts):
+        msg += f"\t[{count+1}] {csg_text}\n"
+    msg += "\n"
+
+    demos = [demo]
+    dataset = prepare_dataset(device, demos, abstract_history, lowlevel_instr_set, tokenizer, skip_label)
+    vlm_input, vlm_media, seed, pre_csg_time_steps = dataset[0]
+    with torch.no_grad():
+        vlm_input['image_embeds'] = img_encoder(vlm_media)
+        result = vlm(**vlm_input, return_dict=True)
+        msg += f"Loss: {result['loss'].item()}\n"
+    msg += "\n"
+
+    msg += test_subgoal_generation(demo, vlm, img_encoder, device, abstract_history, lowlevel_instr_set, tokenizer)
+    return msg
+
+def check_losses(history_type, dataset_size, batch_size, num_epochs, lr, exp_name, models_dir, file):
+
+    train_loss, test_loss = load_losses(models_dir, exp_name)
+
+    title = f"{history_type} History: {dataset_size} demos, batch_size {batch_size}, lr{lr}"
+    fig_fn = f"Losses-{history_type}History-{dataset_size}Demos-Batch{batch_size}-{num_epochs}Epochs-LR{lr}"
+    fig_fp = models_dir + "/" + exp_name + "/" + fig_fn + ".jpg"
+    plot_losses(title, train_loss[:, 0], test_loss[:, 0], num_epochs, fig_fp)
+
+    msg = "[Training and Testing Losses]\n"
+    msg += f"\tTrain Loss: {train_loss[:, 0]}\n"
+    msg += f"\tTest  Loss: {test_loss[:, 0]}\n"
+    print(msg)
+    return msg
+
+def load_models(models_dir, exp_name, model_type="recent"):
+
+    model_dir=os.path.join(models_dir, exp_name)
+
+    # the default value of 'model_type' corresponds to 
+    # models trained by the most recent epoch
+    if model_type == "recent":
+        vlm_fn = "vlm.pt"
+        img_encoder_fn = "image_conv.pt"
+    elif model_type=="best":
+        vlm_fn = "vlm_best.pt"
+        img_encoder_fn = "image_conv_best.pt"
+    elif model_type=="initial":
+        vlm_fn = "vlm_init.pt"
+        img_encoder_fn = "image_conv_init.pt"
+    else:
+        raise f"Unsupported model type: {model_type}"
+    
+    vlm_fp = os.path.join(model_dir, vlm_fn)
+    img_encoder_fp = os.path.join(model_dir, img_encoder_fn)
+
+    if os.path.exists(vlm_fp) and os.path.exists(img_encoder_fp):        
+        vlm = torch.load(vlm_fp)
+        img_encoder =  torch.load(img_encoder_fp)
+
+        model = {"vlm":vlm, "img_encoder":img_encoder}
+    else:
+        model = None
+
+    return model
+
+def load_three_types_of_models(models_dir, exp_name):
+
+    models = {}
+
+    model_types = ["recent", "best", "initial"]
+    for model_type in model_types:
+        model = load_models(models_dir, exp_name, model_type)
+        if model is not None:
+            models[model_type] = model
+
+    return models
+
+def compare_three_types_of_models(demo, models, device, abstract_history, lowlevel_instr_set, tokenizer, skip_label):
+
+    msg = f"\n[Testing 'best', 'recent' and 'initial' models with the demostration with the seed {demo[-1]}]\n"
+    for model_type in ['best', 'recent', 'initial']:
+        if model_type in models:
+            msg += "\n"
+            msg += f"=== Testing {model_type} model [Start] ===\n"
+            msg += test_with_one_demo(demo, models[model_type], device, abstract_history, lowlevel_instr_set, tokenizer, skip_label)
+            msg += f"=== Testing {model_type} model [Finish] ===\n"
+    return msg
+
+def check_one_experiment(
+    exp_name, abstract_history, dataset_size, batch_size, num_epochs, lr,
+    models_dir, test_demo,
+    lowlevel_instr_set, tokenizer, skip_label,
+    device, file):
+    if abstract_history:
+        history_type = "Abstract"
+    else:
+        history_type = "Full"
+
+    msg = check_losses(history_type, dataset_size, batch_size, num_epochs, lr, exp_name, models_dir, file=file)
+
+    models = load_three_types_of_models(models_dir, exp_name)
+    msg += "\n"
+    msg += compare_three_types_of_models(test_demo, models, device, abstract_history, lowlevel_instr_set, tokenizer, skip_label)
+    print(msg, file=file)
